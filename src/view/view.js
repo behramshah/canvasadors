8a8cdd124174aac3fdc1c232e6d6e587fadd3f8f
xorunjiy97 <xorunjiy.a@gmail.com>
2020-12-14 17:04:10 +0400

    done

--- src/view/view.js
+++ src/view/view.js
@@ -1,3 +1,25 @@
 class View {
-    constructor () {}
+    constructor () {
+		this.canvas = null;
+	 };
+	 
+	 init = () => {
+		 this.createCanvas();
+	 }
+
+
+	 createElemntCanvas = props => {
+			const canvas = document.createElement("canvas");
+ 
+			props.className && (canvas.className = props.className);
+			props.id && (canvas.id = props.id);
+			props.height&& (canvas.height = props.height);
+			props.width&& (canvas.width = props.width);
+			
+			return canvas;
+	 }
+
+	 createCanvas = () => {
+		this.canvas = this.createElemntCanvas({className: 'main-container__canvas', id:'canvas', height: '400px', width: '800px' });
+	 }	 
 }
