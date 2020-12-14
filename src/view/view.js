09278110aba04a7eee7f133fe6b07ad55a3bd422
zabrat <zaur1996@bk.ru>
2020-12-14 16:00:41 +0300

    created divEl

--- src/view/view.js
+++ src/view/view.js
@@ -1,11 +1,23 @@
 class View {
     constructor () {
         this.root = null;
-        
+        this.mainContainer = null; 
     }
 
     init = () => {
         this.root = document.getElementById("root");
+        this.mainContainer = this.createDiv({className: "root__main-container", id: "main-container"});
+
+        this.root.append(this.mainContainer);
+    }
+
+    createDiv = (props) => {
+        const div = document.createElement("div");
+
+        props.id && (div.id = props.id);
+        props.className && (div.className = props.className);
+
+        return div;
     }
 
     
