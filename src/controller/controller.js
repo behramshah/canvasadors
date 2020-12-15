3c2b47e278502218fb1509ad2b9b049e2f795f88
behramshah <behramshax@gmail.com>
2020-12-15 14:17:47 +0400

    added get color method

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,3 +1,19 @@
 class Controller {
-    constructor () {}
+    constructor (view) {
+        this.view = view;
+        this.currentColor = null;
+        this.currentRange = null;
+    }
+    
+    init = () => {
+       this.view.init(); 
+       this.view.getColor(this.getCurrentColor.bind(this));
+    }
+
+    getCurrentColor = () => {
+        this.currentColor = this.view.colorInput.value;
+        console.log(this.currentColor)
+    }
 }
+
+export default Controller;
--- src/view/view.js
+++ src/view/view.js
@@ -1,25 +1,64 @@
 class View {
-    constructor () {
-		this.canvas = null;
-	 };
-	 
-	 init = () => {
-		 this.createCanvas();
-	 }
+    constructor() {
+        this.root = null;
+        this.canvas = null;
+        this.mainContainer = null;
+    };
 
+    init = () => {
+        this.root = document.getElementById('root');
+        this.mainContainer = this.createDiv({ className: 'root__main-container', id: 'main-container' });
+        this.canvas = this.createCanvas({ className: 'main-container__canvas', id: 'canvas', height: '400', width: '800' });
+        this.rangeInput = this.createInput({type: 'range', className: 'main-container__range', id: 'range', min: '1', max: '75', step: '1'});
+        this.colorInput = this.createInput({type: 'color', className: 'main-container__color', id: 'color', value:'#ff0000'});
+        this.toolBar = this.createDiv({className: 'main-container__tool-bar', id: 'tool-bar'});
 
-	 createElemntCanvas = props => {
-			const canvas = document.createElement("canvas");
- 
-			props.className && (canvas.className = props.className);
-			props.id && (canvas.id = props.id);
-			props.height&& (canvas.height = props.height);
-			props.width&& (canvas.width = props.width);
-			
-			return canvas;
-	 }
+        this.toolBar.append(this.rangeInput);
+        this.toolBar.append(this.colorInput);
+        this.mainContainer.append(this.canvas);
+        this.mainContainer.append(this.toolBar);
+        this.root.append(this.mainContainer);
+    }
 
-	 createCanvas = () => {
-		this.canvas = this.createElemntCanvas({className: 'main-container__canvas', id:'canvas', height: '400px', width: '800px' });
-	 }	 
+    createDiv = (props) => {
+        const div = document.createElement('div');
+
+        props.id && (div.id = props.id);
+        props.className && (div.className = props.className);
+
+        return div;
+    }
+
+    createCanvas = props => {
+        const canvas = document.createElement('canvas');
+
+        props.className && (canvas.className = props.className);
+        props.id && (canvas.id = props.id);
+        props.height && (canvas.height = props.height);
+        props.width && (canvas.width = props.width);
+
+        return canvas;
+	}
+	
+    createInput = props => {
+        const input = document.createElement('input');
+
+        props.type && (input.type = props.type);
+        props.className && (input.className = props.className);
+        props.id && (input.id = props.id);
+        props.min && (input.min = props.min);
+        props.max && (input.max = props.max);
+		props.step && (input.step = props.step);
+		props.value && (input.value = props.value);
+
+        return input;
+	}
+	
+	getColor = (cb) => {
+        this.colorInput.addEventListener('change', () => {     
+            cb();
+        });
+    }
 }
+
+export default View;
