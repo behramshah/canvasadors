1464d125d8ec9bde52e0fb77c2934194c4b038cc
VictoriaMarjina <61832815+VictoriaMarjina@users.noreply.github.com>
2020-12-15 13:46:43 +0400

    done

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -2,10 +2,17 @@ class Controller {
     constructor (view) {
         this.view = view;
         // this.model = model;
+        this.currentColor = null;
+        this.currentRange = null;
     }
     
     init = () => {
        this.view.init(); 
+       this.view.getRange(this.getCurrentRange.bind(this));
+    }
+
+    getCurrentRange = () => {
+        this.currentRange = this.view.rangeInput.value;
     }
 }
 
--- src/view/view.js
+++ src/view/view.js
@@ -3,20 +3,18 @@ class View {
         this.root = null;
         this.canvas = null;
         this.mainContainer = null;
-        this.currentColor = null;
-        this.currentRange = null;
     };
 
     init = () => {
         this.root = document.getElementById("root");
         this.mainContainer = this.createDiv({ className: "root__main-container", id: "main-container" });
         this.canvas = this.createCanvas({ className: 'main-container__canvas', id: 'canvas', height: '400', width: '800' });
-        this.currentRange = this.createInput({type: 'range', className: 'main-container__range', id: 'range', min: '1', max: '75', step: '1'});
-        this.currentColor = this.createInput({type: 'color', className: 'main-container__color', id: 'color'});
+        this.rangeInput = this.createInput({type: 'range', className: 'main-container__range', id: 'range', min: '1', max: '75', step: '1'});
+        this.colorInput = this.createInput({type: 'color', className: 'main-container__color', id: 'color'});
         this.toolBar = this.createDiv({className: 'main-container__tool-bar', id: 'tool-bar'});
 
-        this.toolBar.append(this.currentRange);
-        this.toolBar.append(this.currentColor);
+        this.toolBar.append(this.rangeInput);
+        this.toolBar.append(this.colorInput);
         this.mainContainer.append(this.canvas);
         this.mainContainer.append(this.toolBar);
         this.root.append(this.mainContainer);
@@ -41,6 +39,7 @@ class View {
 
         return canvas;
     }
+
     createInput = props => {
         const input = document.createElement("input");
 
@@ -52,6 +51,12 @@ class View {
         props.step && (input.step = props.step)
 
         return input;
+    }
+
+    getRange = (cb) => {
+        this.rangeInput.addEventListener('change', () => {     
+            cb();
+        });
     }
 }
 
