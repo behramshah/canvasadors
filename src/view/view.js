101009b7506e97d9ba97969574e27c6bddc319af
zabrat <zaur1996@bk.ru>
2020-12-15 11:44:47 +0300

    created canvas logic

--- src/view/view.js
+++ src/view/view.js
@@ -1,7 +1,9 @@
 class View {
     constructor() {
+        this.ctx = null;
         this.root = null;
         this.canvas = null;
+        this.isPressed = null;
         this.mainContainer = null;
         this.currentColor = null;
         this.currentRange = null;
@@ -11,6 +13,8 @@ class View {
         this.root = document.getElementById("root");
         this.mainContainer = this.createDiv({ className: "root__main-container", id: "main-container" });
         this.canvas = this.createCanvas({ className: 'main-container__canvas', id: 'canvas', height: '400', width: '800' });
+        this.ctx = this.canvas.getContext('2d');
+        this.isPressed = false;
         this.currentRange = this.createInput({type: 'range', className: 'main-container__range', id: 'range', min: '1', max: '75', step: '1'});
         this.currentColor = this.createInput({type: 'color', className: 'main-container__color', id: 'color'});
         this.toolBar = this.createDiv({className: 'main-container__tool-bar', id: 'tool-bar'});
@@ -20,6 +24,27 @@ class View {
         this.mainContainer.append(this.canvas);
         this.mainContainer.append(this.toolBar);
         this.root.append(this.mainContainer);
+    }
+
+    startPosition = event => {
+        this.isPressed = true;
+        this.draw(event);
+    }
+
+    endPosition = () => {
+        this.isPressed = false;
+        this.ctx.beginPath();
+    }
+
+    draw = event => {
+        if(isPressed) {
+            this.ctx.lineWidth = Number(this.currentRange);
+            this.ctx.lineCap = 'round';
+            this.ctx.strokeStyle = this.currentColor;
+
+            this.ctx.lineTo(event.clientX, event.clientY);
+            this.ctx.stroke();
+        }
     }
 
     createDiv = (props) => {
@@ -41,6 +66,7 @@ class View {
 
         return canvas;
     }
+
     createInput = props => {
         const input = document.createElement("input");
 
@@ -49,7 +75,7 @@ class View {
         props.id && (input.id = props.id);
         props.min && (input.min = props.min);
         props.max && (input.max = props.max);
-        props.step && (input.step = props.step)
+        props.step && (input.step = props.step);
 
         return input;
     }
